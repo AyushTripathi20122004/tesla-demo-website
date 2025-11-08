96117864115b269f0e254d623426ff41c7a0aaf8
Aayush Tripaathi <ayushsharma48702@gmail.com>
2025-11-08 13:44:15 +0530

    Adjust card translation and implement pause button
    Updated card container translation and added video pause button functionality.

--- tesla.js
+++ tesla.js
@@ -18,11 +18,27 @@ buttons[0].style.backgroundColor="#171a20"
 buttons.forEach((button, i) => {
     button.addEventListener("click", () => {
         // Move the container, not the NodeList
-        cardContainer.style.transform = `translateX(-${i * 600}px)`; // adjust 400px per card width
+        cardContainer.style.transform = `translateX(-${i * 750}px)`; // adjust 400px per card width
 
         // Optional: update button styles
         buttons.forEach(btn => btn.style.backgroundColor = "");
         button.style.backgroundColor = "#171a20";
     });
 });
+
+
+// video pause buttton functionality
+function pause() {
+  let btn = document.getElementById("pauseBtn");
+
+  if (btn.classList.contains("fa-pause")) {
+    // Pause → Play
+    btn.classList.remove("fa-pause");
+    btn.classList.add("fa-play");
+  } else {
+    // Play → Pause
+    btn.classList.remove("fa-play");
+    btn.classList.add("fa-pause");
+  }
+}
 
