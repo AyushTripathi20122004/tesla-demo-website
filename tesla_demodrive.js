a1fcc53ad08d71670dd37b49d6fed3c3cedbcfcf
Aayush Tripaathi <ayushsharma48702@gmail.com>
2026-02-18 17:21:07 +0530

    Add functions to handle car model button clicks

--- tesla_demodrive.js
+++ tesla_demodrive.js
@@ -1,1 +1,31 @@
+// function for all cards here
+function modelY(){
+    console.log("modelY btn was clicked");
+    let imgage=document.querySelector(".car_images");
+    
+    imgage.style.backgroundImage='url("https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Card-Model-Y-Standard-Desktop-Dynamic-Asset.jpg")';
+}
+function model3(){
+    console.log("model3 btn was clicked");
+    let imgage=document.querySelector(".car_images");
+    imgage.style.backgroundImage='url("https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Card-Model-3-Standard-Desktop.jpg")';
+}
+function cbt(){
+    console.log("model3 btn was clicked");
+    let imgage=document.querySelector(".car_images");
+    imgage.style.backgroundImage='url("https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Card-Cybertruck-Desktop-US-v2.jpg")';
+}
+function modelX(){
+    console.log("model3 btn was clicked");
+    let imgage=document.querySelector(".car_images");
+    imgage.style.backgroundImage='url("https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Card-Model-X-New-Desktop-US-v4.jpg")';
+}
+
+function modelS(){
+    console.log("model3 btn was clicked");
+    let imgage=document.querySelector(".car_images");
+    imgage.style.backgroundImage='url("https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Card-Model-S-Desktop-US-v3.jpg")';
+}
+
+
 
