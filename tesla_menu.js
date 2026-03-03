8d247d29f29ce3a441f21c4dbd86fff9afeac9d0
Aayush Tripaathi <ayushsharma48702@gmail.com>
2026-03-03 22:46:47 +0530

    Update tesla_menu.js

--- tesla_menu.js
+++ tesla_menu.js
@@ -2,3 +2,9 @@
 function close_window(){
     window.location.href="index.html";
 }
+
+// ----- sign in button ------
+function signin() {
+  window.location.href = "tesla_signIn.html";
+  console.log("sign in button was clicked");
+}
