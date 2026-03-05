50e80af1fb3e3f660fe5b10591dfee550b1a67f1
Aayush Tripaathi <ayushsharma48702@gmail.com>
2026-03-05 13:24:48 +0530

    Update tesla_signIn.js

--- tesla_signIn.js
+++ tesla_signIn.js
@@ -2,3 +2,10 @@
 function open_menu_section() {
   window.location.href = "tesla_menu.html";
 }
+
+// create button onclick
+function createAccount() {
+  window.location.href = "tesla_createAccount.html";
+  console.log("create account button was clicked");
+}
+
