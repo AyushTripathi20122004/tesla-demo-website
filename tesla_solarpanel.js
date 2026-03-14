f578f15d27f97596d497a78219e2f06a6b8e66f7
Aayush Tripaathi <ayushsharma48702@gmail.com>
2026-03-14 12:48:08 +0530

    Update tesla_solarpanel.js

--- tesla_solarpanel.js
+++ tesla_solarpanel.js
@@ -1,5 +1,18 @@
+// nav logo tranformation
+let nav_logo=document.querySelector(".logo");
+nav_logo.addEventListener("click",()=>{
+  window.location.href = "index.html";
+});
+
 // ----- menu button ------
 function open_menu_section() {
   window.location.href = "tesla_menu.html";
+}
+
+
+// ----- sign in button ------
+function signin() {
+  window.location.href = "tesla_signIn.html";
+  console.log("sign in button was clicked");
 }
 
