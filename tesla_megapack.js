8bc49b1fadb4119c27a66ac2287b7253e4ea5a89
Aayush Tripaathi <ayushsharma48702@gmail.com>
2026-04-20 13:15:15 +0530

    Implement scrollable card feature with buttons
    Added functionality for scrolling cards with buttons and navigation.

--- tesla_megapack.js
+++ tesla_megapack.js
@@ -1,3 +1,5 @@
+
+
 // nav logo tranformation
 let nav_logo=document.querySelector(".logo");
 nav_logo.addEventListener("click",()=>{
@@ -16,6 +18,86 @@ lang_btn.addEventListener("click",()=>{
     }
 
 });
+
+
+
+
+//-------------ScrollCard_btn creation--------------
+
+let scrollCard = document.querySelectorAll(".s_card");
+let ScrollCard_btn = document.querySelector(".scrollCard_btn");
+
+scrollCard.forEach((_,j)=>{
+    let btn=document.createElement("button");
+    btn.classList.add("scrollBtn")
+    btn.classList.add("rounded-full");
+    btn.classList.add("bg-gray-300");
+    btn.classList.add("p-2");
+    ScrollCard_btn.appendChild(btn);
+    console.log('btn created');
+    
+});
+
+let index=0 // common index
+
+let mg_card4Scroll=document.querySelector(".mg_card4Scroll")
+
+
+
+let Btn=document.querySelectorAll(".scrollBtn")
+Btn[0].style.backgroundColor="#5c5e62"
+
+Btn.forEach((btn,i)=>{
+    btn.addEventListener("click",()=>{
+    console.log('btn clicked')
+    index=i
+    scrollCard_fcn()
+})
+})
+
+let btnCard=document.querySelector(".btnCard")
+let leftBtn=document.querySelector(".left_btn")
+let RightBtn=document.querySelector(".right_btn")
+
+
+leftBtn.addEventListener("click",()=>{
+    if (index>0){
+        index-=1
+        scrollCard_fcn() 
+    }
+})
+
+RightBtn.addEventListener("click",()=>{
+    if (index<4){
+        index+=1
+        scrollCard_fcn() 
+    }
+})
+
+
+// scrolling window updation  function
+let scrollCard_fcn=()=>{
+    mg_card4Scroll.style.transform=`translateX(-${index*20}%)`;
+    Btn.forEach(b=>b.style.backgroundColor="");
+    Btn[index].style.backgroundColor="#5c5e62";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // ----- menu button ------
 function open_menu_section() {
